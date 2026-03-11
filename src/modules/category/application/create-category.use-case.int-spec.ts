d6d1a07e9bc46c8191341b067725b29517600471
Leonardo Rodrigues <ldrigues.dev@gmail.com>
2026-03-11 02:43:38 +0000

    feat: add test integration in category use-case

---
+++ src/modules/category/application/create-category.use-case.int-spec.ts
@@ -0,0 +1,65 @@
+import { id } from "../../../../jest.config";
+import { Uuid } from "../../../shared/domain/value-object/uuid.value-object";
+import { setupSequelize } from "../../../shared/infra/testing/helpers";
+import { CategoryInMemoryRepository } from "../infra/db/in-memory/category-in-memory.repository";
+import { CategorySequelizeRepository } from "../infra/db/sequelize/category-sequelize.repository";
+import { CategoryModel } from "../infra/db/sequelize/category.model";
+import { CreateCategoryUseCase } from "./create-category.use-case";
+
+describe('CreateCategoryUseCase Integration Test', () => {
+  let useCase: CreateCategoryUseCase;
+  let repository: CategorySequelizeRepository
+
+  setupSequelize({ models: [CategoryModel] });
+
+  beforeEach(() => {
+    repository = new CategorySequelizeRepository(CategoryModel);
+    useCase = new CreateCategoryUseCase(repository);
+  })
+
+  it('should create a category', async () => {
+    let output = await useCase.execute({ name: 'Movie' });
+    let entity = await repository.findById(new Uuid(output.id));
+
+    expect(output).toStrictEqual({
+      id: entity.category_id.id,
+      name: 'Movie',
+      description: null,
+      is_active: true,
+      created_at: entity.created_at,
+    })
+
+    output = await useCase.execute({ name: 'Movie', description: 'some description', is_active: false });
+    entity = await repository.findById(new Uuid(output.id));
+
+    expect(output).toStrictEqual({
+      id: entity.category_id.id,
+      name: 'Movie',
+      description: 'some description',
+      is_active: false,
+      created_at: entity.created_at,
+    })
+
+    output = await useCase.execute({ name: 'Movie', description: 'some description' });
+    entity = await repository.findById(new Uuid(output.id));
+
+    expect(output).toStrictEqual({
+      id: entity.category_id.id,
+      name: 'Movie',
+      description: 'some description',
+      is_active: true,
+      created_at: entity.created_at,
+    })
+
+    output = await useCase.execute({ name: 'Movie', is_active: false });
+    entity = await repository.findById(new Uuid(output.id));
+
+    expect(output).toStrictEqual({
+      id: entity.category_id.id,
+      name: 'Movie',
+      description: null,
+      is_active: false,
+      created_at: entity.created_at,
+    })
+  })
+})
