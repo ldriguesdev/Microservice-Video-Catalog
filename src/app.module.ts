4352c6a44b992cf677432e11e316dc98b3f393a7
Leonardo Rodrigues <ldrigues.dev@gmail.com>
2026-04-30 02:42:20 +0000

    feat: update devcontainer with REST client and remove shared module; add example API requests and environment configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,12 @@ import { Module } from '@nestjs/common';
 import { CategoriesModule } from './nest-modules/modules/categories/categories.module';
 import { DatabaseModule } from './nest-modules/modules/database/database.module';
 import { ConfigModule } from './nest-modules/modules/config/config.module';
-import { SharedModule } from './nest-modules/modules/shared/collection.presenter';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     DatabaseModule,
     CategoriesModule,
-    SharedModule,
   ],
   controllers: [],
   providers: [],
